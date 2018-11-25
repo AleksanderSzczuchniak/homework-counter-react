63706b81bcbc093169817fd3fc6dcb7d24e0f712
AleksanderSzczuchniak <alex.szczuchniak@gmail.com>
2018-11-25 16:18:25 +0100

    add and less function

--- src/Counter.js
+++ src/Counter.js
@@ -2,12 +2,27 @@ import React from 'react'
 import Button from './Button'
 
 
+
 class Counter extends React.Component {
-    render() {
-        return (
-            
-        )
+    state = {
+        number: this.props.startNumber || 0
+    }
+    addOneHandler = () => {
+        this.setState({ number: this.state.number + 1 })
+    }
+    lessOneHandler = () => {
+        this.setState({ number: this.state.number - 1 })
+    }
+    addFiveHandler = () => {
+        this.setState({ number: this.state.number + 5 })
+    }
+    lessFiveHandler = () => {
+        this.setState({ number: this.state.number - 5 })
+    }
+    zeroHandler = () => {
+        this.setState({ number: 0 })
     }
 }
+
 
 export default Counter
